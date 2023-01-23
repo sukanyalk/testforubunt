0229d7789ac5bc37b161193df84f6eceba023a6f
manuh <manuh@LAPTOP-EU21NM8B>
2023-01-22 19:53:44 -0500

    second commit

--- src/testing1/myclass.java
+++ src/testing1/myclass.java
@@ -5,7 +5,7 @@ public class myclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("my test got sucess");
-
+System.out.println("second test sucess");
 	}
 
 }
